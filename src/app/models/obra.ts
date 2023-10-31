7f237a1b3b698b7f314f5cd6b96ffb3a4db88861
AndreiBack <andreigregoriback@gmail.com>
2023-10-30 23:17:20 -0300

    service e models de usuario e obra criados

---
+++ src/app/models/obra.ts
@@ -0,0 +1,21 @@
+export class Obra {
+ id: number;
+titulo: string;
+cep: string;
+licitacao: string;
+dataCertame: Date;
+valorEdital: number;
+bairro: string;
+rua: string;
+numeroEndereco: number;
+//voluntarios: Usuario[];
+//foto: Envio;
+valorContratado: number;
+dataInicio: Date;
+dataTermino: Date;
+numeroContrato: number;
+//empresaContratada: Empresa;
+//situacao: Situacao;
+//tipoObra: TipoObra;
+}
+
