83368b0503e34f2d4c869d2cf15c64b711715fa0
Luis Rodriguez <luiserr1394@gmail.com>
2023-11-02 16:56:02 -0300

    Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnvioListComponent } from './components/envio/envio-list/envio-list.component';
+import { EnvioDetailsComponent } from './components/envio/envio-details/envio-details.component';
+import { LoginComponent } from './components/sistema/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"login", pathMatch:"full"},
+  {path:"login", component: LoginComponent},
+  {path:"envios", component: EnvioListComponent},
+  {path:"envios/details", component: EnvioDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EnvioListComponent } from './components/envio/envio-list/envio-list.component';
 import { EnvioDetailsComponent } from './components/envio/envio-details/envio-details.component';
+import { ObraDetailsComponent } from './components/obra/obra-details/obra-details.component';
+import { ObraListComponent } from './components/obra/obra-list/obra-list.component';
+import { LoginComponent } from './components/sistema/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EnvioListComponent,
-    EnvioDetailsComponent
+    EnvioDetailsComponent,
+    ObraDetailsComponent,
+    ObraListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
