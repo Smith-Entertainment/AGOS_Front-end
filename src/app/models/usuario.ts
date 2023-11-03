5c3ebb7628c4bda1ba7c9bca616f47a636810b53
AndreiBack <andreigregoriback@gmail.com>
2023-11-02 23:02:38 -0300

    arrumei as models de obra e usuario

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,16 +1,16 @@
 import { Obra } from "./obra";
 
 export class Usuario {
-    id: number;
-    nome: string;
-    cpf: string;
-    email: string;
-    celular: string;
-    tituloEleitor: string;
-    senha: string;
-    nomePai: string;
-    nomeMae: string;
-    dataNascimento: string; // Você pode ajustar o tipo de data de acordo com o formato que deseja
-    obras: Obra[];
+    id !: number;
+    nome !: string;
+    cpf !: string;
+    email !: string;
+    celular !: string;
+    tituloEleitor !: string;
+    senha !: string;
+    nomePai !: string;
+    nomeMae !: string;
+    dataNascimento !: string; // Você pode ajustar o tipo de data de acordo com o formato que deseja
+    obras !: Obra[];
     //envios: Envio[];
 }
