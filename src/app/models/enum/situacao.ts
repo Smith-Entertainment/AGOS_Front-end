9b6294c281f3deba601b67cf6c126da0aa60f11e
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-11-05 23:46:31 -0300

    copiei models andrei

---
+++ src/app/models/enum/situacao.ts
@@ -0,0 +1,6 @@
+export enum Situacao {
+    EM_HOMOLOGACAO,
+    EM_ANDAMENTO,
+    ENTREGUE,
+    PARALISADA
+}
--- src/app/models/obra.ts
+++ src/app/models/obra.ts
@@ -1,21 +1,29 @@
+import { Bairro } from "./bairro";
+import { Empresa } from "./empresa";
+import { Situacao } from "./enum/situacao";
+import { TipoObra } from "./enum/tipo-obra";
+import { Envio } from "./envio";
+import { Usuario } from "./usuario";
+
 export class Obra {
  id !: number;
 titulo !: string;
+objetivo !: string;
 cep !: string;
 licitacao !: string;
 dataCertame !: Date;
 valorEdital !: number;
-bairro !: string;
+bairro !: Bairro;
 rua !: string;
 numeroEndereco !: number;
-//voluntarios: Usuario[];
-//foto: Envio;
+voluntarios !: Usuario[];
+foto!: Envio;
 valorContratado !: number;
 dataInicio !: Date;
 dataTermino !: Date;
 numeroContrato !: number;
-//empresaContratada: Empresa;
-//situacao: Situacao;
-//tipoObra: TipoObra;
+empresaContratada !: Empresa;
+situacao !: Situacao;
+tipoObra !: TipoObra;
 }
 
