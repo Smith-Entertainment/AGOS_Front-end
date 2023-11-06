c82633ff3bb32cd42a3a5d1a68219a77f63f18f0
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-11-06 01:38:44 -0300

    rotas funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
-import { EnvioDetailsComponent } from './components/envio/envio-details/envio-details.component';
-import { EnvioListComponent } from './components/envio/envio-list/envio-list.component';
 
 const routes: Routes = [
-  {path: "obras/list", component: ListComponent},
-  {path: "obras/form", component: EnvioDetailsComponent},
-  {path: "obras/profile", component: EnvioListComponent}
+  {path: "", redirectTo: "list/obra", pathMatch: "full"},
+  {path: "list/:type", component: ListComponent}
 ];
 
 @NgModule({
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Obra } from 'src/app/models/obra';
 import { ObraService } from 'src/app/service/obra.service';
 
@@ -9,15 +10,23 @@ import { ObraService } from 'src/app/service/obra.service';
 })
 export class ListComponent {
   obraService = inject(ObraService);
+  route = inject(ActivatedRoute);
+
+  listType!: string | null;
 
   obras: Array<Obra> = new Array<Obra>();
 
   constructor(){
-    this.findAll();
-    console.log(this.obras);
+    this.route.paramMap.subscribe(params => {
+      this.listType = params.get("type");
+    })
+
+    if(this.listType == "obra"){
+      this.findAllObras();
+    }
   }
 
-  findAll(){
+  findAllObras(){
     this.obraService.findAll().subscribe({
       next: response => {this.obras = response},
       error: erro => console.log(erro)
