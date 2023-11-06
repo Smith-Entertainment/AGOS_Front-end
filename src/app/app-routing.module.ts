b9b6d6d98157c11b228c658dac4a94196f213035
Frederico Enzo <portifolio.frederico@gmail.com>
2023-11-06 19:17:25 -0300

    sla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ObraFormComponent } from './components/obra-group/obra-form/obra-form.component';
 
 
 const routes: Routes = [
- 
+ {path: 'obra/form', component: ObraFormComponent}
 ];
 
 @NgModule({
